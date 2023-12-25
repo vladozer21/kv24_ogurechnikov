89da0ada7f6814ea19d669ac92de40143a345455
Владислав <about_create@mail.ru>
2023-12-25 15:18:22 +0400

    Добавлен object по 4 лекции
    (cherry picked from commit d0d9f36e465062376d77e86c54e98a44ecb16772)

--- kv24_learning/src/main/scala/lecture_4.scala
+++ kv24_learning/src/main/scala/lecture_4.scala
@@ -1,9 +1,28 @@
 import lecture_3._
 
+
 object lecture_4 extends App {
 
   //Напишите функцию, для вычисления площади каждой фигуры
+  def findFigureArea(fig: Any): Any ={
+    val res = fig match {
+      case fig: Circle =>  s"Площадь круга: ${fig.findArea}"
+      case fig: Rectangle => s"Площадь прямоугольника: ${fig.findArea}"
+      case fig: Square => s"Площадь квадрата: ${fig.findArea}"
+      case _ => "Фигура не опознана"
+    }
+    res
+  }
 
+
+  val circle = Circle(3)
+  val square = Square(5)
+  val rectangle = Rectangle(4, 5)
+
+
+  println(findFigureArea(circle))
+  println(findFigureArea(square))
+  println(findFigureArea(rectangle))
 
 
 }
