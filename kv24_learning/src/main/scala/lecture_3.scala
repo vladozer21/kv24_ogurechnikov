10f8abe4540ece3417cf59001439f6c65b5dfc29
Владислав <about_create@mail.ru>
2023-12-25 13:13:25 +0400

    Добавлео 1 задание по 3 лекции

--- kv24_learning/src/main/scala/lecture_3.scala
+++ kv24_learning/src/main/scala/lecture_3.scala
@@ -1,3 +1,26 @@
 object lecture_3  extends App{
 
+  //Создайте класс Book с полями "заголовок", "автор" и "год издания". Создайте companion object для этого класса, в котором
+  // будет метод createBook, принимающий название, автора и год выпуска, и возвращающий объект Book с этими значениями
+  class Book(val heading: String, val author: String, val yearOfPublic: String)
+    object Book {
+      def createBook(heading: String, author: String, yearOfPublic: String): Book = {
+        new Book(heading, author, yearOfPublic)
+
+      }
+    }
+
+   val res = Book.createBook("Scala для нетерпеливых", "Кей Хорстман", "2017")
+    println()
+    println(s"Название: ${res.heading}")
+    println(s"Автор: ${res.author}")
+    println(s"Год: ${res.yearOfPublic}")
+    println()
+
+
+
+
+  
+
+
 }
