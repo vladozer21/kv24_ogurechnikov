1c3501264a5e7a4fe3df109fc2fef630ee718160
Владислав <about_create@mail.ru>
2023-12-25 20:24:37 +0400

    Добавлено 2 задание по 4 лекции

--- kv24_learning/src/main/scala/lecture_4.scala
+++ kv24_learning/src/main/scala/lecture_4.scala
@@ -9,7 +9,7 @@ object lecture_4 extends App {
       case fig: Circle =>  s"Площадь круга: ${fig.findArea}"
       case fig: Rectangle => s"Площадь прямоугольника: ${fig.findArea}"
       case fig: Square => s"Площадь квадрата: ${fig.findArea}"
-      case _ => "Фигура не опознана"
+      case _ => "Фигура не распознана"
     }
     res
   }
@@ -23,6 +23,29 @@ object lecture_4 extends App {
   println(findFigureArea(circle))
   println(findFigureArea(square))
   println(findFigureArea(rectangle))
+  println(findFigureArea(""))
+  println()
+
+
+    // Напишите функцию, которая проверяет, содержит ли Option[String] определенное слово.
+    // Если содержит, то программа должна вывести "Слово найдено", иначе "Слово не найдено"
+
+
+  def wordChek(option: Option[String], word: String): Unit = {
+    val res: Unit = option match {
+      case Some(s) => if (s.contains(word)) println("Cлово найдено") else println("Cлово не найдено")
+      case _ => println("Неправильные данные")
+    }
+    res
+  }
+
+  val str = "слово"
+  val opt1: Option[String] = Some("Здесь есть слово")
+  val opt2: Option[String] = Some("А тут нет")
+
+  wordChek(opt1, str)
+  wordChek(opt2, str)
+
 
 
 }
