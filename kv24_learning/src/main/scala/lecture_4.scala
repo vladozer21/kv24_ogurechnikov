f5583b9f46997835428235e28298e1ca8ececa1b
Владислав <about_create@mail.ru>
2023-12-26 11:11:33 +0400

    Добавлено 3 задание по 4 лекции

--- kv24_learning/src/main/scala/lecture_4.scala
+++ kv24_learning/src/main/scala/lecture_4.scala
@@ -47,5 +47,35 @@ object lecture_4 extends App {
   wordChek(opt2, str)
 
 
+    //Напишите функцию, которая принимает на вход Option[Int] с возрастом человека
+    // и выводит категорию возраста (ребенок, подросток, взрослый и т. д.)
 
+
+
+  def oldCategory(option: Option[Int]): Unit = {
+    val old = option.getOrElse(-1)
+    val res: Unit = old match {
+      case old => if (12 > old && old >= 0) println("Ребенок")
+                  if (18 > old && old >= 12) println("Подросток")
+                  if (old >= 18) println("Взрослый")
+                  if (old < 0) println("Ошибка ввода возраста")
+
+
+    }
+    res
+  }
+
+  val opt_1: Option[Int] = Some(4)
+  val opt_2: Option[Int] = Some(15)
+  val opt_3: Option[Int] = Some(60)
+  val opt_4: Option[Int] = None
+  val opt_5: Option[Int] = Some(-3)
+
+  println()
+  oldCategory(opt_1)
+  oldCategory(opt_2)
+  oldCategory(opt_3)
+  oldCategory(opt_4)
+  oldCategory(opt_5)
+  println()
 }
